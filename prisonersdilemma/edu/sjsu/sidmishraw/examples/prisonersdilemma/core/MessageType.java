a73ce486b9e7034e86aee18196e4d44f68260189
Sidharth Mishra <sidmishraw@gmail.com>
2017-05-08 18:50:30 -0700

    started prisonersdilemma

---
+++ prisonersdilemma/edu/sjsu/sidmishraw/examples/prisonersdilemma/core/MessageType.java
@@ -0,0 +1,20 @@
+/**
+ *	Project:	AgencyPlatform
+ *  Package:	edu.sjsu.sidmishraw.examples.prisonersdilemma.core
+ *  File:		MessageType.java
+ * 
+ *  @author 			sidmishraw
+ *  Last modified: 		May 8, 2017 6:44:01 PM
+ */
+package edu.sjsu.sidmishraw.examples.prisonersdilemma.core;
+
+/**
+ * @author 			sidmishraw
+ *
+ * Qualified Name: 	edu.sjsu.sidmishraw.examples.prisonersdilemma.core.MessageType
+ *
+ */
+public enum MessageType {
+	
+	DEFECT, COOPERATE
+}
